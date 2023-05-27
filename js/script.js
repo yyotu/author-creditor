b61a148b7b8d803fbc69289247471a303da60815
yyotu <sokolovyne@mail.ru>
2023-05-27 14:40:10 +0300

    endfixmaybe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 const burgerBtn = document.querySelector(".header__burger-btn");
-const headerNavigation = document.querySelector(".header__container");
+const headerNavigation = document.querySelector(".header__navigation");
+
+burgerBtn.addEventListener("click", function() {
+    headerNavigation.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+})
+
+
+console.log(burgerBtn)
 
 
